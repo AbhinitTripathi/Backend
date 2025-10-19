811dabac09b0c503f3215c81fe60143384a5aeee
Abhinit Tripathi <114798873+AbhinitTripathi@users.noreply.github.com>
2025-10-19 19:08:59 +0530

    Add listeners and emitters

--- logger/logger.js
+++ logger/logger.js
@@ -16,3 +16,14 @@ const log_to_file = (event) => {
     const log_message = `${new Date().toISOString()} - ${event.message}`;
     fs.appendFileSync(log_file, log_message);
 }
+
+// Costantly istening to an event named "message"
+logger.on("message", log_to_file);
+
+setInterval(() => {
+    const memory_usage = (os.freemem() / os.totalmem()) * 100;
+    logger.log(`Current Memory Usage: ${ memory_usage.toFixed(2) }`);
+}, 3000)
+
+logger.log("Application started");
+logger.log("Application event occured");
