a1011bb2b42150bd4d0f559a0ea03bd5954f8a36
Abhinit Tripathi <114798873+AbhinitTripathi@users.noreply.github.com>
2025-10-19 18:05:53 +0530

    Add logger class and member functions

--- logger/logger.js
+++ logger/logger.js
@@ -2,4 +2,17 @@ const fs = require("fs");
 const os = require("os");
 
 const EventEmitter = require("events");
-const my_emitter = new EventEmitter();
+
+class Logger extends EventEmitter {
+    log(message) {
+        this.emit("Message", { message });
+    }
+}
+
+const logger = new Logger();
+const log_file = "./eventlog.txt";
+
+const log_to_file = (event) => {
+    const log_message = `${new Date().toISOString()} - ${event.message}`;
+    fs.appendFileSync(log_file, log_message);
+}
